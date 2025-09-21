a652906fa69f77cd2d75efb12ef7fd99b82bb23f
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2025-09-21 15:22:50 +0200

    Update 00.prepare.sh
    Add annotation of input file

--- 04.gene_expression_analyses/03.expression_imputation/00.prepare.sh
+++ 04.gene_expression_analyses/03.expression_imputation/00.prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # data preparation
-awk 'NF==3{print $1}' /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/02.MP2/02.stat/02.imputation/01.genotype/discovery/main/ind_tis2_sample_melt25.tsv | sed '1d' | sort -u > select.txt
-cat select.txt | sort -u | sed '1iSUBJID' | csvtk join -t -f 'SUBJID;SUBJID' - ../meta_raw.txt > meta.txt
-head -1 /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v3.min25_split_impute/observed/express.csv > express.csv # file format: individual,tissue,expression matrix of all genes (inverse normal transformed TMM)
+awk 'NF==3{print $1}' /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/02.MP2/02.stat/02.imputation/01.genotype/discovery/main/ind_tis2_sample_melt25.tsv | sed '1d' | sort -u > select.txt # individual list
+cat select.txt | sort -u | sed '1iSUBJID' | csvtk join -t -f 'SUBJID;SUBJID' - ../meta_raw.txt > meta.txt # header: SUBJID, SEX, AGE
+head -1 /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v3.min25_split_impute/observed/express.csv > express.csv # file format: individual, tissue, expression matrix of all genes within tissue (inverse normal transformed TMM)
 grep -wf select.txt /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v3.min25_split_impute/observed/express.csv >> express.csv
 csvtk cut -f2 express.csv | sed -e '1d' | sort | uniq -c | awk '{print $2"\t"$1}' | sort -nk2 -r > tissue.list

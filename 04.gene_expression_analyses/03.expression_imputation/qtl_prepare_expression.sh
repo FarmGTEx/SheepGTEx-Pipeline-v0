ba8cc8df9574bcab34befdb0034d8cb5572a9d70
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2025-09-21 22:32:15 +0200

    Initial commit of the SheepGTEx pipeline V0

--- 04.gene_expression_analyses/03.expression_imputation/qtl_prepare_expression.sh
+++ 04.gene_expression_analyses/03.expression_imputation/qtl_prepare_expression.sh
@@ -2,7 +2,7 @@
 tis=$1
 
 # 1. normalized expression
-/storage/public/home/2020060185/anaconda3/envs/tensorQTL/bin/python /storage/public/home/2020060185/script/eqtl_prepare_expression.py \
+/storage/public/home/2020060185/anaconda3/envs/tensorQTL/bin/python ~/script/eqtl_prepare_expression.py \
 	../../sheep.PCGlnc.gene.merged.tpm.gct ../../sheep.PCGlnc.gene.merged.count.gct \
 	/storage/public/home/2020060185/genome/sheep/reference/sheep.gtf \
 	../../sample_to_participant.list ${tis}/phenotypes/${tis}.raw --chrs ../../chrAuto.list --sample_ids ${tis}/${tis}.samplelist \

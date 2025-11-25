fc49fa35a18aa7b7bb2567244d94b49d972a169c
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2025-11-25 17:00:29 +0100

    Update prepare_indep_qtl.sh
    remove unused commands

--- 05.molQTL_mapping/03.molQTL_annotation/prepare_indep_qtl.sh
+++ 05.molQTL_mapping/03.molQTL_annotation/prepare_indep_qtl.sh
@@ -7,10 +7,4 @@ zcat /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQ
 zcat /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v1.min40_split/${tis}/results/tensorqtl/independent/${tis}.cis_independent_qtl.txt.gz | awk '$NF==3{print $7}' | sort -u > ${tis}/rank/3.txt
 zcat /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v1.min40_split/${tis}/results/tensorqtl/independent/${tis}.cis_independent_qtl.txt.gz | awk '$NF>=4{print $7}' | sed '1d' | sort -u > ${tis}/rank/4.txt
 
-mkdir -p ${tis}/rank/indep4
-zcat ../functional_enrichment/tensorqtl_independent_qtl.txt.gz | awk -v t=$tis '$1==t&&$20>3&&$NF==1{print $8}' | sort -u > ${tis}/rank/indep4/1.txt
-zcat ../functional_enrichment/tensorqtl_independent_qtl.txt.gz | awk -v t=$tis '$1==t&&$20>3&&$NF==2{print $8}' | sort -u > ${tis}/rank/indep4/2.txt
-zcat ../functional_enrichment/tensorqtl_independent_qtl.txt.gz | awk -v t=$tis '$1==t&&$20>3&&$NF==3{print $8}' | sort -u > ${tis}/rank/indep4/3.txt
-zcat ../functional_enrichment/tensorqtl_independent_qtl.txt.gz | awk -v t=$tis '$1==t&&$20>3&&$NF>=4{print $8}' | sort -u > ${tis}/rank/indep4/4.txt
-
 find ${tis}/rank -type f -empty -delete

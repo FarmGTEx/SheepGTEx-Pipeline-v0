a68722a8198f5654f98bdcb3a78f01b25e92a10b
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2025-12-12 23:32:08 +0100

    Update opera_combine.sh
    Delete the comment at the beginning.

--- 06.molQTL_colocalization/opera_combine.sh
+++ 06.molQTL_colocalization/opera_combine.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 tis=$1
 
-## site
-#zcat ${tis}/opera/output/x??.sites.gz | sed '1iTissue\tGene\tNumber' | pigz -c > ${tis}/opera/output/${tis}.stage2.sites.gz
-#
-## prop
-#zcat ${tis}/opera/output/x??.stage2.prop.gz | awk 'NR==1||$1!="Tissue"' | pigz -c > ${tis}/opera/output/${tis}.stage2.prop.gz
-#
-## ppa
-#for i in {1..6}
-#do
-#	echo $i
-#	zcat ${tis}/opera/output/x??.stage2_${i}_expos_assoc.ppa.gz | awk 'NR==1||$1!="Tissue"' | pigz -c > ${tis}/opera/output/${tis}.stage2_${i}_expos_assoc.ppa.gz
-#done
-#
-## combo
-#zcat ${tis}/opera/output/x??.stage2_combo.res.gz | pigz -c > ${tis}/opera/output/${tis}.stage2_combo.res.gz
+# site
+zcat ${tis}/opera/output/x??.sites.gz | sed '1iTissue\tGene\tNumber' | pigz -c > ${tis}/opera/output/${tis}.stage2.sites.gz
+
+# prop
+zcat ${tis}/opera/output/x??.stage2.prop.gz | awk 'NR==1||$1!="Tissue"' | pigz -c > ${tis}/opera/output/${tis}.stage2.prop.gz
+
+# ppa
+for i in {1..6}
+do
+	echo $i
+	zcat ${tis}/opera/output/x??.stage2_${i}_expos_assoc.ppa.gz | awk 'NR==1||$1!="Tissue"' | pigz -c > ${tis}/opera/output/${tis}.stage2_${i}_expos_assoc.ppa.gz
+done
+
+# combo
+zcat ${tis}/opera/output/x??.stage2_combo.res.gz | pigz -c > ${tis}/opera/output/${tis}.stage2_combo.res.gz
 
 # get the proportion of the eGenes (GWAS/QTL) explained by xQTLs
 python /storage/public/home/2020060185/script/opera_prop.py \

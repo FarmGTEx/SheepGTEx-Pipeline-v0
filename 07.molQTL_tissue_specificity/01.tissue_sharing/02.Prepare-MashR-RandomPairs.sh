9704e1188356a5db4a75279853393036c80b8bea
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-01-16 16:53:44 +0100

    Update 02.Prepare-MashR-RandomPairs.sh

--- 07.molQTL_tissue_specificity/01.tissue_sharing/02.Prepare-MashR-RandomPairs.sh
+++ 07.molQTL_tissue_specificity/01.tissue_sharing/02.Prepare-MashR-RandomPairs.sh
@@ -5,7 +5,7 @@ mkdir -p input/random log
 ### 1. output random nominal SNP-gene pairs information from nominal results to a .txt file
 # (colnames: phenotype_id,variant_id,chr,pos)
 # random select 100000 SNP-gene pairs in each tissue
-for tis in `cut -f1 ../tissue40.list | sed '1d'`
+for tis in `cut -f1 /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/tissue40.list | sed '1d'`
 do
 	jsub -q normal -n 1 -R "span[hosts=1]" -J combine_signif_pairs_${tis} -e log/02.combine_signif_pairs.${tis}.%J.log -o log/02.combine_signif_pairs.${tis}.%J.log \
         "zcat /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v1.min40_split/${tis}/results/tensorqtl/nominal/${tis}.cis_qtl_pairs.chr*.txt.gz | cut -f1,2 | grep -v '^phenotype_id' | shuf -n 100000 > input/random/${tis}.nominal_paires.shuf.txt"
@@ -15,7 +15,7 @@ awk -v OFS="\t" '{gsub(/_/, "\t",$2);print}' input/nominal_paires.shuf.txt | sor
 #> output file: nominal_pairs.combined_signifpairs.txt.gz
 
 ### 2. extract pairs from nominal results for each tissue
-for tis in `cut -f1 ../tissue40.list | sed '1d'`
+for tis in `cut -f1 /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/tissue40.list | sed '1d'`
 do
     # extract_pairs
     jsub -q normal -n 1 -R "span[hosts=1]" -J extract_pairs_${tis} -e log/02.extract_pairs.${tis}.%J.log -o log/02.extract_pairs.${tis}.%J.log \

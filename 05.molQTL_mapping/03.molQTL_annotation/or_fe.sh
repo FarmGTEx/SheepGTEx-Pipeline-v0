5ca57770a623e3984279b5604959411558f4147f
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-03-15 01:30:49 +0100

    Update or_fe.sh

--- 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
+++ 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
@@ -3,8 +3,5 @@ tis=$1
 qtl=$2
 
 python /storage/public/home/2020060185/script/OR_FE.py \
-        /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/${tis}/${qtl} \
-        /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/annotation \
-        /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/allsnp.txt \
-        /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/${tis}/${qtl} \
+        ${tis}/${qtl} annotation allsnp.txt ${tis}/${qtl} \
         --maf_ld_file chrAuto.filtered.keep.score.ld

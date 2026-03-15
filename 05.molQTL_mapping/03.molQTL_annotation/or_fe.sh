2feefd71cd5a19888a5b39aa9c115cd61893180a
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-03-15 01:15:09 +0100

    Update or_fe.sh

--- 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
+++ 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
@@ -7,4 +7,4 @@ python /storage/public/home/2020060185/script/OR_FE.py \
         /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/annotation \
         /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/allsnp.txt \
         /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/${tis}/${qtl} \
-        --maf_ld_file /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/maf_ld.txt
+        --maf_ld_file chrAuto.filtered.keep.score.ld

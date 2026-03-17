d7a6203ca3250b2128a72fbbfb99b29839a2e52d
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-03-17 17:08:29 +0100

    Update 01.jsub.sh

--- 02.RNA-seq/01.quantification/01.jsub.sh
+++ 02.RNA-seq/01.quantification/01.jsub.sh
@@ -3,4 +3,4 @@
 job=$1
 jsub -q fat -M 32000000 -n 1 -R "span[hosts=1]" -J sheep.MP.${job} \
 		-e Logs/sheep.${job}.%J.log -o Logs/sheep.${job}.%J.log \
-	       	"bash 02.command.sh ${job}"
+	       	"bash 01.command.sh ${job}"

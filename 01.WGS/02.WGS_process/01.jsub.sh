b7dc5a07405b3496f8af786ab5b23e81a41d03e9
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-03-21 11:32:14 +0100

    Create Logs directory if it doesn't exist

--- 01.WGS/02.WGS_process/01.jsub.sh
+++ 01.WGS/02.WGS_process/01.jsub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 job=$1
-mkdir -p Logs
+
 # SNP calling 
 jsub -q fat -M 32000000 -n 1 -R "span[hosts=1]" -J wgs_snakemake.${job} \
 		-e Logs/snakemake.${job}.%J.log -o Logs/snakemake.${job}.%J.log \

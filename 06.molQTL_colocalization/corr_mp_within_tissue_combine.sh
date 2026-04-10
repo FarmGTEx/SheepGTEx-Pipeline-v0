07cce35f11777fa7a642ed6046514f5ef5c6f6ad
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-04-10 16:01:40 +0200

    Delete 06.molQTL_colocalization directory

--- 06.molQTL_colocalization/corr_mp_within_tissue_combine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tis=$1
-
-for qtl in sQTL eeQTL isoQTL stQTL 3aQTL enQTL
-do
-	awk -v q=$qtl 'NR>1{print q"\t"$0}' ${tis}/results/mp_within/eQTL_${qtl}.corr.txt
-done > ${tis}/results/mp_within/mp.corr.txt

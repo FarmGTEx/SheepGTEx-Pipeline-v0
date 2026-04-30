ec45d5999ec42300f6b52aab6200c638d5a17a94
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-04-30 13:30:19 +0200

    Update or_fe.sh

--- 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
+++ 05.molQTL_mapping/03.molQTL_annotation/or_fe.sh
@@ -2,6 +2,6 @@
 tis=$1
 qtl=$2
 
-python /storage/public/home/2020060185/script/OR_FE.py \
+python ~/script/OR_FE.py \
         ${tis}/${qtl} annotation allsnp.txt ${tis}/${qtl} \
         --maf_ld_file chrAuto.filtered.keep.score.ld

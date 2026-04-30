6b69aee4ca3d47566ddd4ac810b6b0bdff4f93e0
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2026-04-30 14:08:15 +0200

    Delete 10.population_genetics/03.enrichment directory

--- 10.population_genetics/03.enrichment/or_fe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-i=$1
-
-python ~/script/OR_FE.py \
-	lfsr${i}/tissues annotation /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/functional_enrichment/allsnp.txt lfsr${i}/tissues \
-	--maf_ld_file /storage/public/home/2020060185/00.sheep_goatGTEx/01.sheepGTEx/03.QTL/01.eQTL/v9.breed_specific/admixture/chrAuto.filtered.keep.score.ld
